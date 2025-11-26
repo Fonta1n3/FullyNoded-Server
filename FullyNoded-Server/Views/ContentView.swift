3fc4431f605a193a5cc5a21d6498eb1373e69b66
Fonta1n3 <fontainedenton@googlemail.com>
2025-11-26 07:58:09 +0400

    feat: update to Tor 0.4.8.21

--- FullyNoded-Server/Views/ContentView.swift
+++ FullyNoded-Server/Views/ContentView.swift
@@ -20,7 +20,7 @@ public struct Service: Identifiable {
 
 struct ContentView: View {
     @State private var bitcoinKnotsInstalled = false
-    @State private var torVersion = "v0.4.8.20"
+    @State private var torVersion = "v0.4.8.21"
     @State private var promptToShowPythonGuide = false
     @State private var isInitialLoad = true
     @State private var isInstallingLightning = false
